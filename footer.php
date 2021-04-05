cff1005654ecc7be2b588432850e5fa6b569cae7
cableclark <cableclark@gmail.com>
2021-04-05 21:53:11 +0200

    changed font sizes

--- footer.php
+++ footer.php
@@ -13,9 +13,16 @@
 		<div class="site-info">
 			<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( '%1$s, %2$s. By %3$s.', 'Miss Albini.' ), 'In the lost and found', date('Y'), '<a href="https://www.github.com/cableclark">Cableclark</a>' );
+				printf( esc_html__( '%1$s, %2$s. ', 'Miss Albini.' ), 'In the lost and found', date('Y') );
 			?>
-		</div><!-- .site-info -->
+		</div>
+		<div >
+		<div class="social-media">
+			<div class="social-icon"> <a href="https://twitter.com/lostandfoundmkd" target="_blank"> <?php echo twitter_icon(); ?> </a> </div>
+			<div class="social-icon"> <a href="https://www.instagram.com/inthelostandfoundmusic/" target="_blank"><?php echo instagram_icon(); ?> </a></div>
+			</div>
+		</div>	
+		<!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 <?php wp_footer(); ?>
--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <?php
 /**
  * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
  * @package Miss Albini
  */
 get_header();
@@ -45,6 +37,24 @@ get_header();
 		?>
 	</main>
 <?php
+$args = array(
+    'posts_per_page' => 1, // we need only the latest post, so get that post only
+    'cat' => '573', // Use the category id, can also replace with category_name which uses category slug
+);
+
+$q = new WP_Query( $args);
+
+if ( $q->have_posts() ) {
+    while ( $q->have_posts() ) {
+    $q->the_post();        
+        //Your template tags and markup like:
+		the_title();
+		the_content();
+		
+    }
+    wp_reset_postdata();
+}
+
 dynamic_sidebar( 'album-of-the-week' );
 get_sidebar();
 get_footer();
